a2c126d58315431891aa48ace404fabdca5824dd
Hugo Lundblad <hugo01.lundblad@gmail.com>
2023-10-15 19:54:38 +0200

    Implement Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,6 +15,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Rigidbody rigidBody = gameObject.GetComponent<Rigidbody>();
+            rigidBody.AddForce(0, 300, 0);
+        }
         transform.Translate(0, 0, speed);
     }
 }
